28d22e3667e7d41273d52a834c5b860e2a525f22
drishyaa5 <drishyaa005@gmail.com>
2025-03-20 19:04:41 +0545

    revert

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,37 @@
-import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-
-dotenv.config();
-connectDB();
+import express from 'express';
+import cors from 'cors';
+import colors from 'colors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import authRoute from './routes/UserRoute.js'
+import docRoute from './routes/DocRoutes.js'
+import pharmaRoute from './routes/PharmacyRoutes.js'
+import cartRoute from './routes/cartRoute.js'
+import connectDB from './config/db.js';
+import appointmentRoute from './routes/AppointmentRoute.js'
+import hospitalRoute from './routes/HospitalRoute.js'
 
 const app = express();
+
+dotenv.config();
+
+connectDB();
+
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-// Import routes
-import authRoute from "./routes/UserRoute.js";
-import docRoute from "./routes/DocRoutes.js";
-import pharmaRoute from "./routes/PharmacyRoutes.js";
-import cartRoute from "./routes/cartRoute.js";
-import appointmentRoute from "./routes/AppointmentRoute.js";
-import hospitalRoute from "./routes/HospitalRoute.js";
 
-// Use routes
-app.use("/api/auth", authRoute);
-app.use("/api/doctors", docRoute);
-app.use("/api/pharmacy", pharmaRoute);
-app.use("/api/cart", cartRoute);
-app.use("/api/appointment", appointmentRoute);
-app.use("/api/hospital", hospitalRoute);
+app.use('/api/auth',authRoute)
+app.use('/api/doctors',docRoute)
+app.use('/api/pharmacy', pharmaRoute)
+app.use('/api/cart', cartRoute)
+app.use('/api/appointment', appointmentRoute)
+app.use('/api/hospital', hospitalRoute)
 
-// Export the handler for Vercel
-export default app;
+
+const PORT = 8000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`.bgGreen.white)
+})
